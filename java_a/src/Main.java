b28184c739d2ed48e103150972aea8756fc5b967
juliecorinne <juliecorinne@gmail.com>
2017-02-01 12:05:53 -0500

    Add files via upload

---
+++ java_a/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int i = 0;
+	int j = 0;
+	char asterisk = '#';
+
+	for (i=0; i < 8; ++i) {
+
+	   for (j=1; j < i + 1; ++j) {
+		System.out.print(asterisk);
+
+
+        }
+        System.out.println(asterisk);
+   }
+    }
+}
+
+//System.out.println(asterisk);
+
+
+//System.out.print(asterisk);
+//System.out.println(asterisk);
+
+//System.out.print(asterisk);
+//System.out.print(asterisk);
+//System.out.println(asterisk);
